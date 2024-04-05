0cd4818be51b361a963ea3349c0d2ec1d06ca46e
omartins-zs <gabrielmatheus209@gmail.com>
2024-04-05 15:20:18 -0300

    Add Imports e Tipagens e Add null e Obrigatorio

--- src/app/stockOutputs/page.tsx
+++ src/app/stockOutputs/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Box,
   Button,
@@ -13,31 +14,33 @@ import {
   Tr,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import Header from "../components/Header";
-import Sidebar from "../components/Sidebar";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import { Entry, Product } from "@/types";
 
 const StockOutputs = () => {
   const [amount, setAmount] = useState("");
   const [product_id, setProduct_id] = useState("0");
-  const [listStockOutputs, setStockOutputs] = useState([]);
-  const [listProducts, setListProducts] = useState([]);
+  const [listStockOutputs, setStockOutputs] = useState<Entry[]>([]);
+  const [listProducts, setListProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     const db_stock_outputs = localStorage.getItem("db_stock_outputs")
-      ? JSON.parse(localStorage.getItem("db_stock_outputs"))
+      ? JSON.parse(localStorage.getItem("db_stock_outputs")!)
       : [];
+    console.log(listStockOutputs, localStorage.getItem("db_stock_outputs"));
 
     setStockOutputs(db_stock_outputs);
 
     const db_products = localStorage.getItem("db_products")
-      ? JSON.parse(localStorage.getItem("db_products"))
+      ? JSON.parse(localStorage.getItem("db_products")!)
       : [];
 
     setListProducts(db_products);
   }, []);
 
   const handleNewOutput = () => {
-    if (!amount | (product_id === "0")) {
+    if (!amount || (product_id === "0")) {
       return alert("Selecione o produto e a quantidade!");
     }
 
@@ -46,33 +49,33 @@ const StockOutputs = () => {
     if (listStockOutputs && listStockOutputs.length) {
       localStorage.setItem(
         "db_stock_outputs",
-        JSON.stringify([...listStockOutputs, { id, amount, product_id }])
+        JSON.stringify([...listStockOutputs, { id, amount: Number(amount), product_id }])
       );
 
-      setStockOutputs([...listStockOutputs, { id, amount, product_id }]);
+      setStockOutputs([...listStockOutputs, { id, amount: Number(amount), product_id }]);
     } else {
       localStorage.setItem(
         "db_stock_outputs",
-        JSON.stringify([{ id, amount, product_id }])
+        JSON.stringify([{ id, amount: Number(amount), product_id }])
       );
 
-      setStockOutputs([{ id, amount, product_id }]);
+      setStockOutputs([{ id, amount: Number(amount), product_id }]);
     }
 
     setAmount("");
     setProduct_id("0");
   };
 
-  const removeOutput = (id) => {
-    const newArray = listStockOutputs.filter((item) => item.id !== id);
+  const removeOutput = (id: string) => {
+    const newArray = listStockOutputs.filter((item) => item.product_id !== id);
 
     localStorage.setItem("db_stock_outputs", JSON.stringify(newArray));
 
     setStockOutputs(newArray);
   };
 
-  const getProductById = (id) => {
-    return listProducts.filter((item) => item.id === id)[0]?.name;
+  const getProductById = (id: string) => {
+    return listProducts.filter((item) => item.product_id === id)[0]?.product_name || 'teste';
   };
 
   return (
@@ -92,8 +95,8 @@ const StockOutputs = () => {
               {listProducts &&
                 listProducts.length > 0 &&
                 listProducts.map((item, i) => (
-                  <option key={i} value={item.id}>
-                    {item.name}
+                  <option key={i} value={item.product_id}>
+                    {item.product_name}
                   </option>
                 ))}
             </Select>
@@ -133,7 +136,7 @@ const StockOutputs = () => {
                         fontSize={11}
                         color="red.500"
                         fontWeight="bold"
-                        onClick={() => removeOutput(item.id)}
+                        onClick={() => removeOutput(item.product_id)}
                       >
                         DELETAR
                       </Button>
