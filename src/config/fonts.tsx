9cf6f53539b54b2454cff9681ee819a32095d349
omartins-zs <gabrielmatheus209@gmail.com>
2024-04-08 10:14:05 -0300

    Ajustando as Fonts e Add Font Nunito

--- src/config/fonts.tsx
+++ src/config/fonts.tsx
@@ -1,10 +1,16 @@
+import { Poppins, Merriweather, Nunito } from "next/font/google";
 
-// import { Poppins as Font } from "next/font/google";
+export const fontPoppins = Poppins({
+    subsets: ["latin"],
+    weight: ["100", "300", "400", "500", "700", "900"]
+});
 
-// const font = Font({ subsets: ["latin"], weight: ["100", "300", "400", "500", "700", "900"] });
+export const fontMerriweather = Merriweather({
+    subsets: ["latin"],
+    weight: ["300", "400", "700", "900"]
+});
 
-import { Merriweather as Font } from "next/font/google";
-
-const font = Font({ subsets: ["latin"], weight: ["300", "400", "700", "900"] });
-
-export default font;
+export const fontNunito = Nunito({
+    subsets: ["latin"],
+    weight: ["200", "300", "400", "500", "600", "700", "800"]
+});
